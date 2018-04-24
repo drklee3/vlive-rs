3bd1ad29f8285931372f36e4e6d99948520011e4
drklee3 <dlee3@scu.edu>
2018-04-24 16:05:37 -0700

    Add model docs

--- src/model/channel.rs
+++ src/model/channel.rs
@@ -4,12 +4,16 @@ use chrono::{
 };
 use model::helpers::*;
 
+/// Type of channel, basic or CHANNEL+
 #[derive(Deserialize, Clone, Debug)]
 pub enum ChannelType {
+    /// Regular channels
     BASIC,
+    /// CHANNEL+ channels
     PREMIUM,
 }
 
+/// List of all channels on VLive
 #[derive(Deserialize, Debug)]
 pub struct ChannelList(pub Vec<ChannelListItem>);
 
@@ -26,7 +30,7 @@ impl ChannelList {
             })
             .cloned()
     }
-
+    /// Searches for all channels that contain a string.  This is case insensitive.
     pub fn find_channels<T: AsRef<str>>(&self, search: T) -> Vec<ChannelListItem> {
         let mut found = self.0.clone();
         found.retain(|chan| {
@@ -42,58 +46,91 @@ impl ChannelList {
     }
 }
 
+/// Channel Item return in list of all available channels
 #[derive(Deserialize, Clone, Debug)]
 pub struct ChannelListItem {
+    /// Name of channel
     pub name: String,           // "BTS",
+    /// Icon URl for channel
 	pub icon: String,           // "http://v.phinf.naver.net/20180406_39/1522940433294kxJHw_PNG/profile13_15775.png?type=round58_58",
 	
+    /// Type of channel
     #[serde(rename = "type")]
     pub channel_type: ChannelType,      // "BASIC",
+    /// Channel code used in URLs
 	pub code: Option<String>,           // "FE619"
 }
 
+/// Information on a channel
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct ChannelInfo {
+    /// Channel ID, used in API queries.
     pub channel_seq: u32,
+    /// Channel ID of basic channel if this is a CHANNEL+ channel.
     pub basic_channel_seq: u32,
+    /// Channel code, used in URLs.
     pub channel_code: String,
+    /// Type of channel.
     pub channel_plus_type: ChannelType,
+    /// Name of channel.
     pub channel_name: String,
+    /// Color of channel.
     pub representative_color: String,
+    /// Channel profile image
     pub channel_profile_image: String,
+    /// Channel background color
     pub background_color: String,
+    /// Channel color image
     pub channel_cover_image: String,
+    /// Number of followers
     pub fan_count: u32,
+    /// Description of page, usually just "Welcome to {name} Channel!"
     pub comment: String,
+    /// Usually empty
     pub prohibited_word_like: String,
+    /// Usually empty
     pub prohibited_word_exact: String,
+    /// Image used for sharing
     pub sns_share_img: String,
 
     #[serde(deserialize_with = "bool_from_str")]
     pub banner_show_yn: bool,             //  "N"
+    /// QR code that gives a link to this channel
     pub qrcode: String,
 
+    /// If there are upcoming videos
     #[serde(deserialize_with = "bool_from_str")]
     pub upcoming_show_yn: bool,           //  "N"
 }
 
+/// Information on a video
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct VideoListItem {
+    /// The ID of the video, used in the URL.
     pub video_seq: u32,                      //  57788
+    /// Type of video, either VOD or LIVE
     pub video_type: String,                  //  "VOD"
+    /// Title of the video.
     pub title: String,                       //  "[1년 전 오늘의 TWICE] 지효야 1년 …’s birthday a year ago)"
+    /// Number of plays this video has.
     pub play_count: u32,                     //  46169
+    /// Number of likes or hearts this video has.
     pub like_count: u32,                     //  1387373
+    /// Number of comments this video has.
     pub comment_count: u32,                  //  6179
+    /// The thumbnail URL for this video.
     pub thumbnail: String,                   //  "http://v.phinf.naver.net…6_20.%BD%BA%C6%BF002.jpg"
     pub pick_sort_order: u32,                //  0
+    /// Screen orientation of this video, either `HORIZONTAL` or `VERTICAL`
     pub screen_orientation: String,          //  "HORIZONTAL"
 
+    /// Upload date of this video(?)  This video may not have been visible at this point.
     #[serde(deserialize_with = "timestamp_from_str")]
     pub will_start_at: DateTime<FixedOffset>,               //  "2018-02-01 20:39:00"
 
+    /// End time of this video, usually sometime in 2099 so this isn't really useful.
     #[serde(deserialize_with = "timestamp_from_str")]
     pub will_end_at: DateTime<FixedOffset>,                 //  "2099-12-31 23:59:59"
     
@@ -102,20 +139,28 @@ pub struct VideoListItem {
     pub created_at: Option<DateTime<FixedOffset>>,          //  "2018-04-06 13:35:09"
     pub upcoming_yn: String,                 //  "N"
 
+    /// If this is a "special" live video.
     #[serde(deserialize_with = "bool_from_str")]
     pub special_live_yn: bool,               //  "N"
 
+    /// If this video has a live thumbnail.
     #[serde(deserialize_with = "bool_from_str")]
     pub live_thumb_yn: bool,                 //  "N"
+    /// VLIVE+ product ID if this is a VLIVE+ video, otherwise it's empty.
     pub product_id: String,                  //  ""
+    /// VLIVE+ package ID if this is a VLIVE+ video, otherwise it's empty.
     pub package_product_id: String,          //  ""
-    pub product_type: String,                //  "NONE"
+    /// If this is a VLIVE+ product video.
+    pub product_type: String,                //  "NONE" / "PAID"
+    /// Duration of the video in seconds.
     pub play_time: u32,                      //  199
 
+    /// If this is a basic or CHANNEL+ video
     #[serde(deserialize_with = "bool_from_str")]
     pub channel_plus_public_yn: bool,        //  "N"
     pub expose_status: String,               //  "EXPOSED"
     
+    /// Date when this video was available.
     #[serde(deserialize_with = "timestamp_from_str")]
     pub on_air_start_at: DateTime<FixedOffset>,             //  "2018-02-01 20:44:00"
 }
@@ -132,28 +177,35 @@ impl VideoListItem {
     }
 }
 
+/// Upcoming videos for a channel
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct ChannelUpcomingVideoList {
-    /// Count of upcoming videos.
+    /// Total count of upcoming videos.
     pub total_video_count: u32,
     /// Upcoming video list, this will be None when there are no upcoming videos.
     pub video_list: Option<Vec<VideoListItem>>,
 }
 
+/// Wrapper for upcoming video list.
 #[derive(Deserialize, Debug)]
 pub struct ChannelUpcomingVideoListResult {
     pub result: ChannelUpcomingVideoList,
 }
 
+/// A list of a channel's videos
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct ChannelVideoList {
+    /// Information about this channel
     pub channel_info: ChannelInfo,
+    /// Total videos this channel has.
     pub total_video_count: u32,           //  724
+    /// List of videos
     pub video_list: Vec<VideoListItem>,
 }
 
+/// Wrapper for channel video list.
 #[derive(Deserialize, Debug)]
 pub struct ChannelVideoListResult {
     pub result: ChannelVideoList,
--- src/model/video.rs
+++ src/model/video.rs
@@ -51,34 +51,47 @@ pub struct VideoMeta {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct EncodingOption {
+    /// Resolution name and dimension code
     pub id: String,         // "144P_256_100_64"
+    /// Resolution name
     pub name: String,       // "144P"
+    /// Encoding profile, usually `BASE` for low resolution videos or `HIGH`
     pub profile: String,    // "BASE"
+    /// Width of video resolution
     pub width: u32,         // 250
+    /// Height of video resolution
     pub height: u32,        // 144
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Bitrate {
+    /// Video bitrate in kbps
     pub video: f64,
+    /// Audio bitrate in kbps
     pub audio: f64,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct VideoItem {
+    /// ID of video
     pub id: String,             // "E49EC1F9611925347CFD30D5494F10929821",
     #[serde(rename = "useP2P")]
     pub use_p2p: bool,           // false,
     pub duration: f64,          // 620.538,
     pub preview_duration: Option<u32>,   // 30,
+    /// Size of video in bytes
     pub size: u64,              // 12880198,
+    /// Encoding codec name
     #[serde(rename = "type")]
     pub type_: String,           // "avc1",
+    /// Encoding options of video
     pub encoding_option: EncodingOption,
+    /// Video and audio bitrate
     pub bitrate: Bitrate,
     pub p2p_meta_url: String,     // "",
     pub p2p_url: String,         // "",
+    /// URL to the mp4 file.  This expires after an unknown amount of time.
     pub source: String,         // "http://globalv.p.naverrmc.edgesuite.net/global/read/global_v_2018_02_01_4/a36b6bbd-0734-11e8-89b9-0000000049b9.mp4?__gda__=1517618848_e4d87e6279f0e75cd59d483e6523a0e9"
 }
 
@@ -96,49 +109,67 @@ pub struct Key {
     #[serde(rename = "type")]
     pub type_: String,       // "param",
     pub name: String,       // "__gda__",
+    /// Key value of video, required to get video data.
     pub value: String       // "1517618848_c6b0e8d115c8e780999621c9b8b0dfe7"
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Stream {
+    /// Type of stream, usually `HLS`
     #[serde(rename = "type")]
     pub type_: String,   // "HLS",
     pub key: Key,
+    /// m3u8 URL for streaming
     pub source: String, // "http://globalv.p.naverrmc.edgesuite.net/global/read/global_v_2018_02_01_4/hls/f4740c94-0734-11e8-8062-0000000041ed.m3u8"
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Caption {
+    /// Language of caption, such as `en`
     pub language: String,   // "en",
+    /// Country of language, such as `US`
     pub country: String,    // "US",
+    /// Locale code
     pub locale: String,     // "en_US",
+    /// Label of caption used in the UI, such as `English`
     pub label: String,      // "English",
+    /// Full URL of .vtt caption file
     pub source: String,     // "http://caption.rmcnmv.naver.net/globalv/global_meta/read/global_v_2018_02_01_3/09c42cb8-0741-11e8-8582-3ca82a214e91-1517483655295_en_US_cp.vtt?__gda__=1517618848_dc2c8b243b42c43632d58757677a189a"
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Captions {
+    /// Default / selected caption language(?)
     pub caption_lang: String,
+    /// List of all available captions
     pub list: Vec<Caption>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Thumbnail {
+    /// Time during the video for this thumbnail in seconds
     pub time: f64,
+    /// URL to the .jpg thumbnail file
     pub source: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Thumbnails {
+    /// List of available thumbnails
     pub list: Vec<Thumbnail>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Video {
+    /// Video information
     pub meta: VideoMeta,
+    /// .mp4 videos
     pub videos: Videos,
+    /// .m3u8 streams
     pub streams: Vec<Stream>,
+    /// .vtt captions
     pub captions: Captions,
+    /// .jpg thumbnails
     pub thumbnails: Thumbnails,
 }
